b721ab277d163ce66e3a1d0c635cb7983889c964
root <root@Hirobot.server>
2026-02-06 16:26:41 +0000

    feat: implement neon visual style and UI palette (Issue #6)

--- scripts/TetrominoData.gd
+++ scripts/TetrominoData.gd
@@ -10,30 +10,30 @@ enum Type { I, J, L, O, S, T, Z }
 const DATA = {
 	Type.I: {
 		"cells": [Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0)],
-		"color": Color.CYAN
+		"color": Color(0, 1, 1, 2) # Neon Cyan
 	},
 	Type.J: {
 		"cells": [Vector2i(-1, -1), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0)],
-		"color": Color.BLUE
+		"color": Color(0, 0.5, 1, 2)
 	},
 	Type.L: {
 		"cells": [Vector2i(1, -1), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0)],
-		"color": Color.ORANGE
+		"color": Color(1, 0.5, 0, 2)
 	},
 	Type.O: {
 		"cells": [Vector2i(0, -1), Vector2i(1, -1), Vector2i(0, 0), Vector2i(1, 0)],
-		"color": Color.YELLOW
+		"color": Color(1, 1, 0, 2)
 	},
 	Type.S: {
 		"cells": [Vector2i(0, -1), Vector2i(1, -1), Vector2i(-1, 0), Vector2i(0, 0)],
-		"color": Color.GREEN
+		"color": Color(0, 1, 0, 2)
 	},
 	Type.T: {
 		"cells": [Vector2i(0, -1), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0)],
-		"color": Color.PURPLE
+		"color": Color(1, 0, 1, 2) # Neon Magenta
 	},
 	Type.Z: {
 		"cells": [Vector2i(-1, -1), Vector2i(0, -1), Vector2i(0, 0), Vector2i(1, 0)],
-		"color": Color.RED
+		"color": Color(1, 0, 0, 2)
 	}
 }
